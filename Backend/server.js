d8d2f593f0618ad1cc2e8952d054f41909005bdd
habiba <habibaamasha32@gmail.com>
2025-06-15 22:11:09 +0300

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://blog-website-de4g-kqm4pwgkx-habibas-projects-2c481812.vercel.app"
+      "https://blog-website-de4g-habibas-projects-2c481812.vercel.app/",
+      "https://blog-website-de4g-git-main-habibas-projects-2c481812.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
