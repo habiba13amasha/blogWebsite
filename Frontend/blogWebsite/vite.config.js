f50cec1ffa180c19a67ab2caaf45e8f40cc972d3
habiba <habibaamasha32@gmail.com>
2025-06-16 02:49:24 +0300

    .

--- Frontend/blogWebsite/vite.config.js
+++ Frontend/blogWebsite/vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
 
-   
+    server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 
 })
