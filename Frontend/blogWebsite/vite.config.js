8b04f8ecc8f16f457f2a1af4c9d8dee930fefb5c
habiba <habibaamasha32@gmail.com>
2025-06-16 04:19:33 +0300

    .

--- Frontend/blogWebsite/vite.config.js
+++ Frontend/blogWebsite/vite.config.js
@@ -1,19 +1,4 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: './',
-  plugins: [react(), tailwindcss()],
-
-    server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-
-})
+  base: '/',
+  plugins: [react()],
+k})
