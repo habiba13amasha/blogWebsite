fcabd0e6a2ec2d076aae55ca795bc0d7c0d4df9b
habiba <habibaamasha32@gmail.com>
2025-06-23 01:51:32 +0300

    fix

--- Frontend/blogWebsite/vite.config.js
+++ Frontend/blogWebsite/vite.config.js
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Frontend/blogWebsite/dist/' : '/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist', 
